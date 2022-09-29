702ec114c001e92a29accb993453634709540db4
diktrome <larsroosma15@gmail.com>
2022-09-29 10:02:42 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace WpfApp1_test
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Cquit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void bstart_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 windo = new Window1();
+            this.Close();
+        }
+    }
+}
